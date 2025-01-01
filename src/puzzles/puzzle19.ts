14a4b8d5038adf48cf96516a9bbb553005a81ad5
Neel Rao <neelorao@gmail.com>
2024-12-31 21:12:20 -0800

    refactor: use range

--- src/puzzles/puzzle19.ts
+++ src/puzzles/puzzle19.ts
@@ -1,6 +1,7 @@
 import { Puzzle } from './Puzzle';
 import { getNumbers } from '~/util/parsing';
 import { mod } from '~/util/arithmetic';
+import { range } from '~/util/range';
 
 export const puzzle19 = new Puzzle({
     day: 19,
@@ -52,7 +53,7 @@ export const puzzle19 = new Puzzle({
  * It can actually solve my input, though it takes a long time (16 minutes on my laptop).
  */
 function bruteForceSolvePart2(nElves: number) {
-    const elves = Array.from({ length: nElves }, (_, i) => i + 1);
+    const elves = range(1, nElves + 1);
 
     let i = 0;
     let totalIterations = 0;
